de747a217d0cc33dd38b29560e4cd06824e41156
devback90 <devback90.nexsol@gamil.com>
2025-10-24 09:20:30 +0900

    ref: convert to multi module

---
+++ libs/storage/db-core/src/db-core.module.ts
@@ -0,0 +1,8 @@
+import { Module } from '@nestjs/common';
+import { DbCoreService } from './db-core.service';
+
+@Module({
+  providers: [DbCoreService],
+  exports: [DbCoreService],
+})
+export class DbCoreModule {}
